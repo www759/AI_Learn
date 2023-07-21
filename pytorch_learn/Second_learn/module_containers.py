ddfd1cef92697b2061e15fe6d6d5caabb9e274dd
fei <2629789522@qq.com>
2023-07-21 20:14:31 +0800

    module containers

---
+++ pytorch_learn/Second_learn/module_containers.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+
+
+class LeNetSequential(nn.Module):
+    def __init__(self, classes):
+        super(LeNetSequential, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 6, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(6, 16, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Linear(16*5*5, 120),
+            nn.ReLU(),
+            nn.Linear(120, 84),
+            nn.ReLU(),
+            nn.Linear(84, classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size()[0], -1)
+        x = self.classifier(x)
+        return x
+
+
+model = LeNetSequential(classes=2)
